5383825e405cfc73cd2fba517fb7f27e1517f707
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-01 13:02:45 -0600

    create product form sent to backend

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,40 +1,107 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import DisplayError from './ErrorMessage.js';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ){
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: {
+          create: {
+            image: $image,
+            altText: $name
+          }
+        }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Nice Shoes',
     price: 2423,
     description: 'These are the best shoes',
   });
+  const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, {
+    variables: inputs,
+  });
   
   return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          placeholder="Name" 
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input 
-          type="number" 
-          id="price" 
-          name="price" 
-          placeholder="Price" 
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+    <Form onSubmit={async (e) => {
+      e.preventDefault();
+      console.log(inputs);
+      // submit input fields to backend
+      await createProduct();
+      clearForm();
+    }}>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input 
+            required
+            type="file" 
+            id="image" 
+            name="image" 
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            placeholder="Name" 
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input 
+            type="number" 
+            id="price" 
+            name="price" 
+            placeholder="Price" 
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea 
+            id="description" 
+            name="description" 
+            placeholder="Description" 
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
 
-      <button type="button" onClick={clearForm}>Clear Form</button>
-      <button type="button" onClick={resetForm}>Reset Form</button>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
 
-    </form>
+
+    </Form>
   )
 }
