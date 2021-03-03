32d73bbb0be04c4568d6b109eacaf42b4991c366
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-03 11:59:40 -0600

    created signup flow

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -5,49 +5,53 @@ import { useMutation } from '@apollo/client';
 import { CURRENT_USER_QUERY } from './User';
 import Error from './ErrorMessage';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+    createUser(data: {
+      email: $email,
+      name: $name,
+      password: $password
+    }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
+    name: '',
     password: '',
   });
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, error, loading }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
-    refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }]
   })
   async function handleSubmit(e) {
     e.preventDefault();
-    const res = await signin();
+    const res = await signup().catch(console.error);
     console.log(res);
     resetForm();
   };
-  const error = 
-    data?.authenticateUserWithPassword.__typename === 
-    "UserAuthenticationWithPasswordFailure" 
-      ? data?.authenticateUserWithPassword 
-      : undefined;
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
+      <h2>Sign Up For An Account</h2>
       <Error error={error} />
       <fieldset>
+        {data?.createUser && <p>Signed up with {data.createUser.email} - Please go ahead and sign in!</p>}
+        <label htmlFor="name">
+          Your Name 
+          <input 
+            type="text" 
+            name="name" 
+            placeholder="Your Name" 
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="email">
           Email 
           <input 
