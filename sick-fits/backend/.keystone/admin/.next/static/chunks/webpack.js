8543800f23b3c259b2bb5b042b9e7ee744995021
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-08 12:03:39 -0600

    add to cart button updates cart

--- sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
+++ sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4306cb1bdde342add4c";
+/******/ 	var hotCurrentHash = "0a0272bec9d1a505c9b4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
