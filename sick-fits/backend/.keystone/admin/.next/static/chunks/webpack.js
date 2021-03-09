45a72524e3067bffb7d940272d745ad464c9bda7
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-09 09:39:10 -0600

    search functionality and styling

--- sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
+++ sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a0272bec9d1a505c9b4";
+/******/ 	var hotCurrentHash = "e92864151582985d54db";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- sick-fits/backend/.keystone/admin/.next/static/development/_buildManifest.js
+++ sick-fits/backend/.keystone/admin/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cart-items":["static\u002Fchunks\u002Fpages\u002Fcart-items.js"],"/users":["static\u002Fchunks\u002Fpages\u002Fusers.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcart-items","\u002Fusers"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
