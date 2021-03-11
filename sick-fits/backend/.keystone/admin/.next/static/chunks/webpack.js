7d49ebb08d9602620b958302401945309e57b5c4
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-11 10:43:08 -0600

    keystone role edit based on admin role

--- sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
+++ sick-fits/backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e355902342a4d19cb463";
+/******/ 	var hotCurrentHash = "cc23e0495c43ca517836";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
